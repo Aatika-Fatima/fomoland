c9935b838863e73e8f291266897a6903d35c6bff
Aatika Fatima <csentoes123@gmail.com>
2017-09-15 12:45:19 +0530

    commit 1

---
+++ fomoland/src/main/java/com/fomo/FomolandApplication.java
@@ -0,0 +1,12 @@
+package com.fomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FomolandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FomolandApplication.class, args);
+	}
+}
